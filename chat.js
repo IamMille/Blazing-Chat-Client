d4e1dd1c0b1015aa633592c340967006557871b8
Mille <IamMille@users.noreply.github.com>
2017-03-16 09:52:14 +0100

    updated layout

--- chat.js
+++ chat.js
@@ -2,11 +2,19 @@
 window.addEventListener("load", () => {
 ////////////////////////////////////////
 
+$.noConflict(); // disabe bootstraps jQuery
+
 alertify.logPosition("bottom right");
 alertify.maxLogItems(2);
 
 $("#startChat").addEventListener("click", startChat);
 doLogin();
+
+// var database = firebase.database();
+
+////////////////////////////////////////
+}); // end of load
+// end of load
 
 function startChat()
 {
@@ -20,15 +28,36 @@ function startChat()
 
 function doLoadChat() {
   //$("#chat").innerText = "Hello " + $("#username").value;
+  // load data
+    //loop doAddMessage
+  data.forEach( chatmsg => {
+    doAddMessage(chatmsg);
+  });
+
 }
-function doAddMessage() {
-  /*
-  <div class="msg" data-id="12312">
-    (12:34:59) &lt;<b>username</b>&gt; Ett chatt meddelande ser ut såhär!
-    <span class="glyphicon glyphicon-thumbs-up"></span>
-    <span class="glyphicon glyphicon-thumbs-down"></span>
-  </div>
-  */
+function doAddMessage(oMsg)
+{
+  var keysRequired = ["id", "datetime", "chan", "user", "msg"];
+  if (keysRequired.filter(key => oMsg[key]).length != keysRequired.length) {
+    console.log("doAddMessage(): missing parameters, need", keysRequired.length, "got:", Object.keys(oMsg) ); return; }
+
+  console.log(oMsg);
+  var div = document.createElement("div");
+  div.setAttribute("data-id", oMsg.id);
+  div.classList.add("msg");
+  div.innerHTML = `
+    <div class="icons">
+      <span class="glyphicon glyphicon-thumbs-up"></span>
+      <span class="glyphicon glyphicon-thumbs-down"></span>
+    </div>
+    <div class="text">
+      (${oMsg.datetime.slice(-8)})
+      &lt;<b>${oMsg.user}</b>&gt; ${oMsg.msg}
+    </div>
+  `.trim();
+
+  $("#chatMsgs").appendChild(div);
+
 }
 
 function doLogin()
@@ -40,7 +69,7 @@ function doLogin()
   $("#navLogin").innerHTML = '<li><a href="#">Log Out</a></li>';
   $("#navLogin li:first-child a").addEventListener("click", doLogout);
   $("#inputUsername").style.display = "";
-  $("#chat").style.display = "block";
+  $("#chatWindow").style.display = "block";
 
   console.log("User is logged in.");
   alertify.success("You've been logged in!");
@@ -53,7 +82,7 @@ function doLogout(event)
   sessionStorage.removeItem("username");
   $("#navLogin").innerHTML = "";
   $("#inputUsername").style.display = "block";
-  $("#chat").style.display = "";
+  $("#chatWindow").style.display = "";
   //disable input.
   //disable updates.
   console.log("User is logged out.");
@@ -67,7 +96,3 @@ function $(str)
   else if (els.length > 0) return Array.from(els);
   else return [];
 }
-
-////////////////////////////////////////
-}); // end of load
-// end of load
